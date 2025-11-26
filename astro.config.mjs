7170a430db95459325c733eca66f1aa1f2c0e16f
Yamrc <yamrci@outlook.com>
2025-11-27 00:45:04 +0800

    Feat(style): 更新到`tailwind v4`
    - 更新`tailwind`到v4
    - 替换postcss、astro/tailwind为vite方案
    - 尽可能重建样式

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections";
 import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 import swup from "@swup/astro";
@@ -31,9 +31,6 @@ export default defineConfig({
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
-		tailwind({
-			nesting: true,
-		}),
 		swup({
 			theme: false,
 			animationClass: "transition-swup-", // see https://swup.js.org/options/#animationselector
@@ -157,6 +154,7 @@ export default defineConfig({
 	},
 	vite: {
 		plugins: [
+			tailwindcss(),
 			compression({
 				algorithms: ["brotliCompress"],
 			}),
